d28da7640edf0814b0bf3d499f16288b54cdf8c9
Tony Wang <wangh9947@gmail.com>
2023-06-08 17:56:42 +0800

    q1 2) done

--- src/Components/Counter.js
+++ src/Components/Counter.js
@@ -2,10 +2,16 @@ import { useState } from "react";
 
 const App = () => {
   const [num, setNum] = useState(0);
+  const [numColor, setNumColor] = useState("green");
   return (
-    <div>
+    <div style={{ color: numColor }}>
       <button
         onClick={() => {
+          if (num % 2 !== 0) {
+            setNumColor("green");
+          } else {
+            setNumColor("red");
+          }
           setNum(num + 1);
         }}
       >
@@ -14,6 +20,11 @@ const App = () => {
       <h1>{num}</h1>
       <button
         onClick={() => {
+          if (num % 2 !== 0) {
+            setNumColor("green");
+          } else {
+            setNumColor("red");
+          }
           setNum(num - 1);
         }}
       >
